991d0f2936360db06bf81504d4b9de07bdf64e66
Sovietaced <Sovietaced@gmail.com>
2014-01-05 02:58:02 -0800

    Added full functionality

--- app/controllers/relay_controller.rb
+++ app/controllers/relay_controller.rb
@@ -2,7 +2,16 @@ class RelayController < ApplicationController
 	skip_before_filter  :verify_authenticity_token
 
   def index
-    render :json => Node.first
+    render :json => {:url => Node.first.url}
+  end
+
+  def set
+    if params[:url]
+      Relay.reset_relay(params[:url])
+      node = Node.first
+      node.url = params[:url]
+      node.save!
+    end
   end
 
 end
